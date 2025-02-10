50dd09acd0bf7795e3d68f2b9af651b1ab4d5b68
Akshay Patil <53465888+AkshayPatil4@users.noreply.github.com>
2025-02-10 21:36:23 +0100

    setting chnaged

--- ai_blog/settings.py
+++ ai_blog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--cm6t030gd7u_m3p8^(pubad7vvm=16)e+q5_ed5xu$kq4+%bp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
@@ -137,8 +137,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
